81c67dd44615d1ab42cefd01a92f81c121895acd
Forrest Iandola <forresti@eecs.berkeley.edu>
2015-05-16 15:44:21 -0700

    making 1fps sample

--- demo_video/config_vatic.sh
+++ demo_video/config_vatic.sh
@@ -5,16 +5,23 @@
 
 input_video_dir=~/millennium_nscratch/forresti/vatic_videos/
 input_video=$input_video_dir/bradley_univ_Thu_14_May_2015__06_00_01.mp4
-input_frame_dir=$input_video_dir/bradley_univ_Thu_14_May_2015__06_00_01_frames_5fps
-job_name=bradley_univ
+input_frame_dir=$input_video_dir/bradley_univ_Thu_14_May_2015__06_00_01_frames_1fps
+job_name=bradley_univ_90min
 
 mkdir $input_frame_dir
-turkic extract $input_video $input_frame_dir --fps 5
+turkic extract $input_video $input_frame_dir --fps 1
 
-turkic load $input_video Car \
+turkic load $job_name $input_frame_dir Car \
 --blow-radius 1 \
---train-with $job_name \
---duration 2700 \ #2700 frames (90min @ 0.5 fps)
+--length 5400 \ #5400 frames (90min @ 1 fps)
+
+
+#possibly needed:
+turkic publish --offline
+turkic find --id $job_name #e.g. https://distill.cs.berkeley.edu?id=136&hitId=offline
+
+
+#ignoring "stuff for mturkers"...
 
 #--title "Label the cars in this video (15sec of video). Per-object bonus."
 #--description "Draw boxes around the cars, trucks, and vehicles in this video. When the cars move, you move the boxes. See 'show instructions' in the HIT. Feel free to email us with questions."
@@ -23,7 +30,7 @@ turkic load $input_video Car \
 #--cost 0.05    
 #--per-object-bonus 0.02 
 #--completion-bonus 0.1  
-
+#--train-with $job_name \
 
 #TODO: separate script to...
 #- dump bboxes
